449f5879ed7eef67322999819045d4a7c6c25b04
Kingsly54 <kingslyw@nycstudents.net>
2026-03-02 11:32:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-Macbook={
-    "name":"Macbook Sequoia",
-    "price":"Kidney",
-    "color":"inclusitivity",
+Laptop={
+    "name":"MacBook Air (M4, 13.6‑inch)",
+    "price":"$899",
+    "color":"Silver"
+}
+Pencil={
+    "name":"Dixon Ticonderoga 2B Graphite Pencil, Wood-Cased, Yellow, 12-Pack",
+    "price":"$2.50",
+    "color":"orange"
+}
+Gun={
+    "name":"AK-47 7.62×39mm Semi-Automatic Rifle",
+    "price":"2.00",
+    "color":"black"
+}
+Basketball={
+    "name":"Wilson Evolution Game Basketball, Official Size 29.5",
+    "price":"99.99",
+    "color":"black"
 }
 
-for index, item in enumerate(Macbook):
-    print(index, ":", Macbook["name"])
+idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+if idk == "Basketball":
+    print(Basketball["name"])
+if idk == "Gun":
+    print(Gun["name"])
+if idk == "Laptop":
+    print(Laptop["name"])
+if idk == "Pencil":
+    print(Pencil[0]["name"])
