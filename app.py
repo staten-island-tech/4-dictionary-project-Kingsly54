96c015e79581eb09ee1bd0e0a53d85ed2d04a2d9
Kingsly54 <kingslyw@nycstudents.net>
2026-03-09 09:26:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,29 +18,53 @@ Basketball={
     "price":"99.99",
     "color":"black"
 }
-Cart=[]
-idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
-Num=0
-if idk == "Basketball":
-    print(Basketball["name"])
-    bre=input("Would you like to add to cart?")
-    price=Basketball["price"]
-elif idk == "Gun":
-    print(Gun["name"])
-    bre=input("Would you like to add to cart?")
-    price=Gun["price"]
-elif idk == "Laptop":
-    print(Laptop["name"])
-    bre=input("Would you like to add to cart?")
-    price=Laptop["price"]
-elif idk == "Pencil":
-    print(Pencil["name"])
-    bre=input("Would you like to add to cart?")
-    price=Gun["price"]
-if bre=="Yes" or "yes":
-    Cart.append(idk)
-    Num+=price
-
-    
-print(Cart)
-print(Num)
+def breh():
+    Cart=[]
+    idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+    Num=0
+    if idk == "Basketball":
+        print(Basketball["name"])
+        bre=input("Would you like to add to cart?")
+        pric=Basketball["price"]
+        price=float(pric)
+        if bre=="Yes" or "yes":
+            Num+=price
+            Cart.append(Basketball["name"])
+            Ask=input("Would you like to buy more?")
+            if Ask=="Yes" or "yes":
+                idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+    elif idk == "Gun":
+        print(Gun["name"])
+        bre=input("Would you like to add to cart?")
+        pric=Gun["price"]
+        price=float(pric)
+        if bre=="Yes" or "yes":
+            Num+=price
+            Cart.append(Gun["name"])
+            Ask=input("Would you like to buy more?")
+            if Ask=="Yes" or "yes":
+                idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+    elif idk == "Laptop":
+        print(Laptop["name"])
+        bre=input("Would you like to add to cart?")
+        pric=Laptop["price"]
+        price=float(pric)
+        if bre=="Yes" or "yes":
+            Num+=price
+            Cart.append(Laptop["name"])
+            Ask=input("Would you like to buy more?")
+            if Ask=="Yes" or "yes":
+                idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+    elif idk == "Pencil":
+        print(Pencil["name"])
+        bre=input("Would you like to add to cart?")
+        pric=Pencil["price"]
+        price=float(pric)
+        if bre=="Yes" or "yes":
+            Num+=price
+            Cart.append(Pencil["name"])
+            Ask=input("Would you like to buy more?")
+            if Ask=="Yes" or "yes":
+                idk=input("What would you like to buy Items include: Basketball, Pencil, Gun, Laptop")
+    print(Num)
+    print(Cart)
